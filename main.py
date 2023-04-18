15212e56fa79ccf9b2610fd5cb49bdb73ec2c995
kali <lepeev-v@yandex.ru>
2023-04-18 09:44:45 +0300

    Загрузка с yandex

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import yandex_music
 
 if __name__ == '__main__':
-    pass
+    text = '''
+    1. yandex
+    2. ruy_zvukofon_com
+    3. ru_hitmotop_com
+    4. musify_club
+    5. load_for_list'''
+
+    print(text)
+
+    user_input = input('Введи номер для продолжения: ')
+
+    if user_input == '1':
+        yandex_music.main()
+    elif user_input == '2':
+        pass
+    elif user_input == '3':
+        pass
+    elif user_input == '4':
+        pass
+    elif user_input == '5':
+        pass
